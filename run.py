fe7be48e2ce7994b769523eb023d27cf10a14ac6
Mandy Wang <shunflower320@gmail.com>
2020-06-16 15:34:30 +0800

    removing doc structure from child

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def template_test():
-    return render_template("main.html", my_string="Wheee", my_list=[0, 1, 2, 3, 4, 5])
+    return render_template("main.html", my_string="Wheeeeeeeeeeeee "*100, my_list=[0, 1, 2, 3, 4, 5])
 
 if __name__=='__main__': # to conduct unit test, demo, etc
     app.run(debug=True)
